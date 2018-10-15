087679f018eb94742d3f4d5ac9c4b70e667d3c47
Radek Komorowski <rkomorowski97@gmail.com>
2018-10-16 01:05:14 +0200

    painting whole picture in given color

--- sdl2histogram/asm.asm
+++ sdl2histogram/asm.asm
@@ -1,6 +1,37 @@
+section .data
+width: dd 0
+height: dd 0
+min: dd 255
+max: dd 0
+
 section .text
 bits 32
-global _foo
-_foo:
-	mov eax, 7
+global _function
+_function:
+;eax -height
+;ecx -width
+;edx -pixels
+	push ebp
+	mov ebp, esp
+	mov [width], ecx
+	mov [height], eax
+	mov ecx, 0
+iLoop:
+	cmp ecx, [width]
+	je done
+	mov ebx, 0
+jLoop:
+	;do stuff there
+	mov byte[edx], 54w
+	add edx, 1
+
+	cmp ebx, [height]
+	je jLoopDone
+	inc ebx
+	jmp jLoop
+jLoopDone:
+	inc ecx
+	jmp iLoop
+done:
+	leave
 	ret
